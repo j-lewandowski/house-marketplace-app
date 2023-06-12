0327359286806d5b193a250499bb2562ced98022
Kuba <kubalew.pl@gmail.com>
2023-06-12 17:34:45 +0200

    added Sign in and Sign Up forms

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Route path="/offers" element={<Offers />} />
           <Route path="/profile" element={<SignIn />} />
           <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<SignIn />} />
         </Routes>
         <Navbar />
